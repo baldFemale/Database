00072f0211a705e7b9a9c6c6dde2dcde6faf5b79
shenshopping <34395567+shenshopping@users.noreply.github.com>
2017-12-11 19:48:51 +0800

    Add files via upload

---
+++ src/model/SC.java
@@ -0,0 +1,11 @@
+package model;
+
+public final class SC {
+	public static final String TABLE="user1.sc";
+    public static final String AYEAR="Ayear";
+    public static final String SEMESTER="Semester";
+    public static final String S_ID="S_id";
+    public static final String C_ID="C_id";
+    public static final String SCORE="Score";
+    public static final String GPA="GPA";
+}
---
+++ src/model/Teacher.java
@@ -0,0 +1,14 @@
+package model;
+
+public final class Teacher {
+	public static final String TABLE="user1.teacher";
+    public static final String NAME="T_name";
+    public static final String ID="T_id";
+    public static final String SEX="T_sex";
+    public static final String BIRTH="T_birth";
+    public static final String PROV="T_prov";
+    public static final String REGION="T_region";
+    public static final String DEPT_ID="Dept_id";
+    public static final String PROF="Prof";
+    public static final String SAL="Sal";
+}
