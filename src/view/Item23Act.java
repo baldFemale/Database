ad05d307172c5f7795359fa113331961c5bddb82
shenshopping <34395567+shenshopping@users.noreply.github.com>
2017-12-25 17:36:57 +0800

    更新有关出生日期部分

--- src/view/Item23Act.java
+++ src/view/Item23Act.java
@@ -126,6 +126,15 @@ public final class Item23Act extends JPanel implements  ActionListener{
 		if(this.comboBoxSex.getSelectedItem() != null)
 			sqlString.concat(" and " + Student.SEX + " = '" + comboBoxSex.getSelectedItem() + "'");
 		//TODO 出生日期因格式未知，还未加上去
+		if(this.textBirthMin.getText() != null) {
+			float idMin = Float.parseFloat(textBirthMin.getText());
+			sqlString.concat(" and " + Student.BIRTH + " >= " + idMin);
+		}
+		if(this.textBirthMax.getText() != null) {
+			float idMax = Float.parseFloat(textBirthMax.getText());
+			sqlString.concat(" and" + Student.BIRTH + " <= " + idMax);
+		}
+		
 		if(this.comboBoxProv.getSelectedItem() != null)
 			sqlString.concat(" and " + Student.PROV + " = '" + comboBoxProv.getSelectedItem() + "'");
 		if(this.textIntoMin.getText() != null) {
