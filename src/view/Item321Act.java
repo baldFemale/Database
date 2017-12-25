a22c47570a5130d1f67107e9e70a30e716e6f2d2
baldFemale <33083386+baldFemale@users.noreply.github.com>
2017-12-25 20:25:13 +0800

    Update Item321Act.java

--- src/view/Item321Act.java
+++ src/view/Item321Act.java
@@ -76,7 +76,7 @@ public class Item321Act extends JPanel implements ActionListener{
 		try {
 			ResultSet resultSet = statement.executeQuery(sql);
 			jsp1 = new Table(res).jsp1;
-			jp3.removeALL();
+			jp3.removeAll();
 			jp3.add(jsp1);
 			this.add(jp3);
 			this.updateUI();
